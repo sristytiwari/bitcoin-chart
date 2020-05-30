2d144696e54b21cf2f5177378c16630ad9a89402
sristytiwari <sristytiwari1998@gmail.com>
2020-05-30 13:18:19 +0530

    Updates

---
+++ precache-manifest.4cc3c08b42f433afd8866341c53c9e74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c7a25ddbc4c7e9434fcf03335ca06e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a24c0a567ca729c2b84b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a70957825d1de3ce3448",
+    "url": "/static/js/2.3ae11738.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/static/js/2.3ae11738.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a24c0a567ca729c2b84b",
+    "url": "/static/js/main.09c1c85a.chunk.js"
+  },
+  {
+    "revision": "5028f2bc7569b33ab212",
+    "url": "/static/js/runtime-main.80e5bace.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
